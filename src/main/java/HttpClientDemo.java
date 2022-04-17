7f39cb9d23367933c05870979729cec4944c741a
Mark Freedman <mark.freedman@gmail.com>
2022-04-17 14:03:53 -0400

    add post operation

--- src/main/java/HttpClientDemo.java
+++ src/main/java/HttpClientDemo.java
@@ -1,21 +1,30 @@
 //import org.apache.http.client.HttpClient;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import domain.User;
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 public class HttpClientDemo {
 
-    private static String TEST_URL = "https://gorest.co.in/public/v2/users";
+    private static String TEST_GET_URL = "https://gorest.co.in/public/v2/users";
+    private static String TEST_POST_URL = "https://gorest.co.in/public/v2/users";
+    // TODO - change this value with an API token you acquire yourself.
+    private static String API_TOKEN = "changeme";
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
 
     public void go() {
         OkHttpClient client = new OkHttpClient.Builder()
@@ -24,7 +33,7 @@ public class HttpClientDemo {
                 .build();
 
         Request request = new Request.Builder()
-                .url(TEST_URL)
+                .url(TEST_GET_URL)
                 .get()
                 .build();
 
@@ -34,6 +43,8 @@ public class HttpClientDemo {
             public void onResponse(Call call, Response response)
                     throws IOException {
 
+                System.out.println("Success?: " + response.isSuccessful());
+
                 var objectMapper = new ObjectMapper();
                 var responseBody = response.body();
                 var users = objectMapper.readValue(responseBody.string(), User[].class);
@@ -42,11 +53,44 @@ public class HttpClientDemo {
                 for (var user: users) {
                     System.out.println("User: " + user);
                 }
+                sendPostCommand(client);
             }
 
             public void onFailure(Call call, IOException e) {
                 // error
                 System.err.println("exception getting data: " + e);
+            }
+        });
+    }
+
+
+
+    private void sendPostCommand(OkHttpClient client) throws JsonProcessingException {
+        // email must be unique
+        var user = new User(99, "Wilford Brimley", UUID.randomUUID() + "@brimley.org", "male", "active");
+        var userJson = objectMapper.writeValueAsString(user);
+        RequestBody body = RequestBody.create(MediaType.parse("application/json"), userJson);
+
+        Request request = new Request.Builder()
+                .url(TEST_POST_URL)
+                .header("Authorization", "Bearer " + API_TOKEN)
+                .post(body)
+                .build();
+
+        System.out.println("User json: " + userJson );
+        System.out.println("Sending Post: " + request.toString());
+
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                System.out.println("got response: " + response.code());
+                System.out.println("Success?: " + response.isSuccessful());
+            }
+
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                System.err.println("exception posting data: " + e);
             }
         });
     }
