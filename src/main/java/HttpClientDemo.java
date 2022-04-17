911f99533d840bfd01d0421363e43bd80e58009b
Mark Freedman <mark.freedman@gmail.com>
2022-04-17 14:35:49 -0400

    add more queries

--- src/main/java/HttpClientDemo.java
+++ src/main/java/HttpClientDemo.java
@@ -1,10 +1,9 @@
-//import org.apache.http.client.HttpClient;
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import domain.User;
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.HttpUrl;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -18,15 +17,19 @@ import java.util.concurrent.TimeUnit;
 
 public class HttpClientDemo {
 
+    private static String API_HOST = "gorest.co.in";
     private static String TEST_GET_URL = "https://gorest.co.in/public/v2/users";
     private static String TEST_POST_URL = "https://gorest.co.in/public/v2/users";
     // TODO - change this value with an API token you acquire yourself.
     private static String API_TOKEN = "changeme";
 
+    private static String USER_NAME = "Wilford Brimley";
     private ObjectMapper objectMapper = new ObjectMapper();
 
+    // TODO add check for response code
+    // add query string
 
-    public void go() {
+    public void runQueries() {
         OkHttpClient client = new OkHttpClient.Builder()
                 .readTimeout(1000, TimeUnit.MILLISECONDS)
                 .writeTimeout(1000, TimeUnit.MILLISECONDS)
@@ -44,16 +47,10 @@ public class HttpClientDemo {
                     throws IOException {
 
                 System.out.println("Success?: " + response.isSuccessful());
-
-                var objectMapper = new ObjectMapper();
-                var responseBody = response.body();
-                var users = objectMapper.readValue(responseBody.string(), User[].class);
-
-                System.out.println("Response code:" + response.code());
-                for (var user: users) {
-                    System.out.println("User: " + user);
+                if (response.isSuccessful()) {
+                    printUsersFromResponse(response);
+                    sendPostCommand(client);
                 }
-                sendPostCommand(client);
             }
 
             public void onFailure(Call call, IOException e) {
@@ -67,7 +64,7 @@ public class HttpClientDemo {
 
     private void sendPostCommand(OkHttpClient client) throws JsonProcessingException {
         // email must be unique
-        var user = new User(99, "Wilford Brimley", UUID.randomUUID() + "@brimley.org", "male", "active");
+        var user = new User(99, USER_NAME, UUID.randomUUID() + "@brimley.org", "male", "active");
         var userJson = objectMapper.writeValueAsString(user);
         RequestBody body = RequestBody.create(userJson, MediaType.parse("application/json"));
 
@@ -86,6 +83,9 @@ public class HttpClientDemo {
             public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
                 System.out.println("got response: " + response.code());
                 System.out.println("Success?: " + response.isSuccessful());
+                if (response.isSuccessful()) {
+                    callGetWithQueryParameter(client);
+                }
             }
 
             @Override
@@ -93,5 +93,53 @@ public class HttpClientDemo {
                 System.err.println("exception posting data: " + e);
             }
         });
+    }
+
+    private void callGetWithQueryParameter(OkHttpClient client) {
+
+        HttpUrl httpurl = new HttpUrl.Builder()
+                .scheme("https")
+                .host(API_HOST)
+                .addPathSegment("public")
+                .addPathSegment("v2")
+                .addPathSegment("users")
+                .addQueryParameter("name", USER_NAME)
+                .build();
+
+
+        Request request = new Request.Builder()
+                .url(httpurl.url())
+                .header("Authorization", "Bearer " + API_TOKEN)
+                .build();
+
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                System.out.println("got response: " + response.code());
+                System.out.println("Success?: " + response.isSuccessful());
+                if (response.isSuccessful()) {
+                    printUsersFromResponse(response);
+                }
+            }
+
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                System.err.println("exception posting data: " + e);
+            }
+        });
+    }
+
+    private void printUsersFromResponse(Response response) throws IOException {
+        var responseBody = response.body();
+        var users = objectMapper.readValue(responseBody.string(), User[].class);
+
+        if (response.isSuccessful()) {
+            System.out.println("**** All wilfords:");
+            System.out.println("Response code:" + response.code());
+            for (var user : users) {
+                System.out.println("User: " + user);
+            }
+        }
     }
 }
